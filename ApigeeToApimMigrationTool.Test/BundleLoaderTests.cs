dcc48eb6cb66dd1e807e432550a7eca51403e307
Jason Specland <jaspecla@microsoft.com>
2024-01-12 13:42:12 -0500

    added failing test to test bug with bundles

--- ApigeeToApimMigrationTool.Test/BundleLoaderTests.cs
+++ ApigeeToApimMigrationTool.Test/BundleLoaderTests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using ApigeeToAzureApimMigrationTool.Core;
+using ApigeeToAzureApimMigrationTool.Core.Interface;
+using ApigeeToAzureApimMigrationTool.Service;
+using ApigeeToAzureApimMigrationTool.Service.Transformations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +12,32 @@ namespace ApigeeToApimMigrationTool.Test
 {
     public class BundleLoaderTests
     {
-        [Fact]
-        public Task FileBundleLoader_WithSharedFlowPolicy_LoadsSharedFlow()
+        // The purpose of these tests is to test loading bundles from files.
+        // We're using the test file loaders, but the process should be the same
+        // with the real loaders, only we'll be loading from the filesystem instead 
+        // of from the Apigee API.
+
+        private AzureApimService _azureApimServiceUnderTest;
+        private IBundleProvider _bundleProvider;
+        public BundleLoaderTests()
         {
+            _bundleProvider = new ApigeeFileBundleProvider("TestBundles");
+
+            IApigeeXmlLoader apigeeXmlLoader = new ApigeeXmlFileLoader(_bundleProvider);
+            IApigeeManagementApiService apigeeManagementApiService = new ApigeeManagementApiTestFileService(_bundleProvider, apigeeXmlLoader);
+            IApimProvider apimProvider = new MockApimProvider();
+            IPolicyTransformationFactory policyTransformationFactory = new PolicyTransformationFactory(apigeeManagementApiService, apimProvider, apigeeXmlLoader);
+
+            _azureApimServiceUnderTest = new AzureApimService(
+                apigeeXmlLoader: apigeeXmlLoader,
+                apimProvider: apimProvider,
+                policyTransformer: new ApigeeToApimPolicyTransformer(policyTransformationFactory));
+        }
+        [Fact]
+        public async Task FileBundleLoader_WithSharedFlowPolicy_LoadsSharedFlow()
+        {
+            await _bundleProvider.LoadBundle("Test-API");
+            await _azureApimServiceUnderTest.ImportApi("Test-Apim", "Test-API", null, null, null);
 
         }
     }
